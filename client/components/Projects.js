f32edeef4f5f02e2fd5439d70020459fc69f5c63
Jordy <jordynavalee@gmail.com>
2022-09-29 13:11:21 -0500

    initial commit

---
+++ client/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <>
+      <div>Spotify Project</div>
+      <div>Boba Shoppe</div>
+      <div>CSS Animation Fun</div>
+    </>
+  );
+}
+
+export default Projects;
